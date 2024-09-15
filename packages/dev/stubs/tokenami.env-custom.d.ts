eb4822ee42fa7e1afc6e13c4cbb60169e7d78785
Jenna Smith <175330+jjenzz@users.noreply.github.com>
2024-09-15 14:48:03 +0100

    create nested compose api

--- packages/dev/stubs/tokenami.env-custom.d.ts
+++ packages/dev/stubs/tokenami.env-custom.d.ts
@@ -3,7 +3,7 @@ import config from './tokenami.config.js';
 
 export type Config = typeof config;
 
-declare module '@tokenami/dev' {
+declare module '@tokenami/css' {
   interface TokenamiConfig extends Config {}
   interface TokenamiProperties {}
 }
